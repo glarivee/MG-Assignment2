84438e8e5c1a33df762b02fe85712b0afa09ea76
glarivee <63671414+glarivee@users.noreply.github.com>
2021-04-23 18:16:28 -0400

    I have no idea

---
+++ dev/js/zoomIn.js
@@ -0,0 +1,11 @@
+import { gsap } from "gsap";
+
+export let zoomTL = gsap.timeline();
+
+gsap.set("#BackgroundMoon",{transformOrigin:"top"});
+gsap.set("#ForegroundCrater",{transformOrigin:"top"});
+gsap.set("#CowJetPack",{transformOrigin:"top"});
+
+zoomTL.from("#BackgroundMoon",{duration:3, scale:10},"zoom")
+    .from("#ForegroundCrater",{duration:3, scale:10},"zoom")
+    .from("#CowJetPack",{duration:3, scale:10}, "zoom")
